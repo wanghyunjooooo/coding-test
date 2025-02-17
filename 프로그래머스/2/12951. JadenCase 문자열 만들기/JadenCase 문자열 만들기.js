bcec63d38ce52c615ae4156fd35c50c4163c55b4
wanghyunjooooo <w2324@e-mirim.hs.kr>
2025-02-17 23:50:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let words = s.split(" "); 
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > 0) { 
+            words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+        }
+    }
+    return words.join(" ");
+}
